0607265e1fd304aa90e7334af08413728034f9d3
Ross Nicholls <rjnicholls79@icloud.com>
2019-12-17 09:12:16 +0000

    Working
    Got it Working and in a better format

---
+++ src/components/LogIn.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="LogIn">
+    <transition name="fade">
+<section class="log-in" v-if="isShow['Log']">
+  <h2>please sign in</h2>
+  <input type="input" placeholder="User Name">
+  <input type="password" placeholder="Password">
+  <p class="lostCredentials">Lost Your Credentials</p>
+  <router-link to="/about"><div class="mx-auto d-block log-arrow"><i class="fas fa-chevron-right"></i></div></router-link>
+  <router-view/>
+</section>
+</transition>
+<section class="signIn" v-if="isShow['Sign']">
+<h2 class="text-center">please sign in</h2>
+<div class="mx-auto d-block sign-arrow" @click="isShow['Log'] = !isShow['Log']; isShow['Sign'] = !isShow['Sign']"><i class="fas fa-chevron-down"></i></div>
+</section>
+    <section class="container">
+      <section class="main-illustration">
+        <img alt="House" src="../assets/Img_Asset.png" class="mx-auto d-block">
+      </section><!-- End Main Illustration -->
+      <section class="logo">
+        <img alt="logo" src="../assets/Logo_Asset.png" class="mx-auto d-block">
+      </section><!-- End logo -->
+      <h1 class="text-center mt-2">welcome to our online referral portal</h1>
+    </section><!-- End Container -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LogIn',
+  data(){
+  return{
+    isShow:{
+      'Sign': true,
+      'Log': false,
+    }
+  }
+},
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style lang="scss">
+@import "../styles/universal.scss";
+@import "../styles/log-in.scss";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+    <LogIn></LogIn>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LogIn from '@/components/LogIn.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    LogIn
   }
 }
 </script>
+
+<style lang="scss">
+@import "../styles/universal.scss";
+</style>
