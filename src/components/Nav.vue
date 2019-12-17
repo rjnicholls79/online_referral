88c8f63416d19af0d0620ff533ba0d1858de5c75
Ross Nicholls <rjnicholls79@icloud.com>
2019-12-17 12:00:45 +0000

    wright off
    need to revert back

---
+++ src/components/Nav.vue
@@ -0,0 +1,38 @@
+<template>
+<main id="nav-main">
+  <transition name="slide">
+    <section class="nav-links" v-if="showNav['Hide']">
+      <ul>
+        <router-link to="/"><li><i class="fas fa-unlock"></i>&nbsp; Log Out</li></router-link>
+      <li><i class="fas fa-cog"></i>&nbsp; Settings</li>
+        <li><i class="fas fa-user-plus"></i>&nbsp; New Lead</li>
+        <li><i class="fas fa-user-friends"></i>&nbsp; Refferals</li>
+        <li><i class="fas fa-user-edit"></i>&nbsp; Manage Users</li>
+      </ul>
+    </section>
+  </transition>
+  <section class="row">
+    <div class="col-6">
+      <img alt="logo" src="../assets/Logo_Asset.png" class="nav_logo">
+    </div><!-- End col-6 -->
+    <div class="col-6">
+      <i class="fas fa-bars fa-2x float-right" @click="showNav['Hide'] = !showNav['Hide']; showNav['Show'] = !showNav['Show']"></i>
+    </div><!-- End col-6 -->
+  </section>
+</main>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+  data() {
+    return {
+      showNav: {
+        'Show': true,
+        'Hide': false,
+      }
+    }
+  },
+
+}
+</script>
--- src/views/Welcome_screen.vue
+++ src/views/Welcome_screen.vue
@@ -1,19 +1,19 @@
 <template>
-  <main class="welcome">
-    <Nav></Nav>
-    <section class="main-illustration container">
-      <img alt="House" src="../assets/Img_Asset.png" class="mx-auto d-block">
-      <h1>Welcome {{name}}</h1>
-      <p>Would you like to start a new lead?</p>
-      <Button>New Lead</Button>
-    </section><!-- End Main Illustration -->
-  </main>
+<main class="welcome">
+  <Nav></Nav>
+  <section class="main-illustration container">
+    <img alt="House" src="../assets/Img_Asset.png" class="mx-auto d-block">
+    <h1>Welcome {{name}}</h1>
+    <p>Would you like to start a new lead?</p>
+    <Button>New Lead</Button>
+  </section><!-- End Main Illustration -->
+</main>
 </template>
 
 <script>
 // @ is an alias to /src
 import Button from '@/components/ui/Button.vue'
-import Nav from '@/components/ui/Nav.vue'
+import Nav from '@/components/Nav.vue'
 
 export default {
   name: 'welcome',
@@ -21,11 +21,11 @@ export default {
     Nav,
     Button,
   },
-  data(){
-  return{
-    name:"Forename",
-  }
-},
+  data() {
+    return {
+      name: "Forename",
+    }
+  },
 }
 </script>
 
